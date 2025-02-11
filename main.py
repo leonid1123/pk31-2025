65dd1f1720d019565628600b55ab4c92a18fa1c3
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-02-11 16:59:57 +0300

    start

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import mysql.connector
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window.geometry("400x400+10+10")
 # Connect to server
 cnx = mysql.connector.connect(
     host="127.0.0.1",
@@ -11,7 +16,15 @@ cur = cnx.cursor()
 # Execute a query
 cur.execute("SELECT * FROM phones")
 # Fetch one result
-row = cur.fetchone()
-print(row)
+row = cur.fetchall()
+phones = []
+for item in row:
+    phones.append(f"{item[0]} {item[1]}")
+
+full_list = Variable(value=phones)
+listbox = Listbox(listvariable=full_list, width=50)
+listbox.grid(row=0, column=0)
+window.mainloop()
+
 # Close connection
 cnx.close()
