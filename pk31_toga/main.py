466dd0b3ad2c329a90e870d885748bdea0bf231f
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-07 13:30:27 +0300

    note select

--- pk31_toga/main.py
+++ pk31_toga/main.py
@@ -2,15 +2,15 @@ import toga
 from toga.style.pack import ROW, COLUMN, Pack, RIGHT
 import sqlite3
 
-
+#https://github.com/leonid1123/pk31-2025/tree/master/pk31_toga
 class App(toga.App):
     def button_handler(self, widget):
         tmp = self.login_entry.value
         tmp_password = self.pass_entry.value
-        con = sqlite3.connect("passwords.db")
-        cur = con.cursor()
-        cur.execute("SELECT password FROM users WHERE login=?",(tmp,))
-        ans = cur.fetchall()
+        self.con = sqlite3.connect("passwords.db")
+        self.cur = self.con.cursor()
+        self.cur.execute("SELECT password FROM users WHERE login=?",(tmp,))
+        ans = self.cur.fetchall()
         print(ans)
         if len(ans) > 0:
             p1 = ans[0]
@@ -22,6 +22,8 @@ class App(toga.App):
                 print("неможно")
 
     def startup(self):
+        self.user_id = 2
+        self.style = Pack(padding=5, flex=1, font_family='Helvetica', font_size=16)
         self.notes_window = None
         self.main_window = toga.MainWindow()
         main_box = toga.Box()
@@ -31,11 +33,11 @@ class App(toga.App):
         pass_box = toga.Box()
         pass_box.style.update(direction=ROW)
 
-        button = toga.Button("Вход в систему", on_press=self.button_handler, style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
-        login_label = toga.Label("Логин", style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
-        pass_label = toga.Label("Пароль", style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
-        self.login_entry = toga.TextInput(style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
-        self.pass_entry = toga.PasswordInput(style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
+        button = toga.Button("Вход в систему", on_press=self.button_handler, style=self.style)
+        login_label = toga.Label("Логин", style=self.style)
+        pass_label = toga.Label("Пароль", style=self.style)
+        self.login_entry = toga.TextInput(style=self.style)
+        self.pass_entry = toga.PasswordInput(style=self.style)
         login_box.add(login_label)
         login_box.add(self.login_entry)
         pass_box.add(pass_label)
@@ -51,11 +53,11 @@ class App(toga.App):
             self.notes_window = toga.Window(title="Заметки")
             self.notes_box = toga.Box()
             self.notes_box.style.update(direction=COLUMN)
-            self.note1 = toga.Button("Заметка1")
-            self.note2 = toga.Button("Заметка2")
-            self.note3 = toga.Button("Заметка3")
-            self.note4 = toga.Button("Заметка4")
-            self.note5 = toga.Button("Заметка5")
+            self.note1 = toga.Button("Заметка1", on_press=self.noteShow,style=self.style)
+            self.note2 = toga.Button("Заметка2",style=self.style)
+            self.note3 = toga.Button("Заметка3",style=self.style)
+            self.note4 = toga.Button("Заметка4",style=self.style)
+            self.note5 = toga.Button("Заметка5",style=self.style)
             self.notes_box.add(self.note1)
             self.notes_box.add(self.note2)
             self.notes_box.add(self.note3)
@@ -64,10 +66,23 @@ class App(toga.App):
             self.notes_window.content = self.notes_box
             self.notes_window.show()
 
-    def noteShow(self):
+    def noteShow(self, widget):
         self.note_window = toga.Window(title="Заметка")
         self.note_box = toga.Box()
         self.note_box.style.update(direction=COLUMN)
+        self.note_view = toga.MultilineTextInput(style=self.style)
+        self.note_win_btn = toga.Button("OK",style=self.style)
+        self.note_box.add(self.note_view)
+        self.note_box.add(self.note_win_btn)
+        self.note_window.content = self.note_box
+        self.cur.execute("SELECT note FROM notes WHERE id_user = 2")
+        ans = self.cur.fetchone()
+        print(ans)
+        if len(ans) > 0:
+            self.note_view.value = ans
+
+        self.note_window.show()
+
 
 
 if __name__ == "__main__":
