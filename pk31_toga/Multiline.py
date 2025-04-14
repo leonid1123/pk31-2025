d1aebbb7b4667df88c5f06d3a5d6218c9da0ab72
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-14 13:25:57 +0300

    note select

--- pk31_toga/Multiline.py
+++ pk31_toga/Multiline.py
@@ -1,15 +1,16 @@
 import toga
-from toga.style.pack import ROW, COLUMN, Pack
+from toga.style.pack import COLUMN, Pack
 
 
 class App(toga.App):
     def startup(self) -> None:
+        """основной метод"""
         my_style = Pack(padding=5, flex=1, font_family='Helvetica', font_size=16)
         self.main_window = toga.MainWindow()
-        main_box = toga.Box()
+        main_box = toga.Box(style=my_style)
         main_box.style.update(direction=COLUMN)
         self.entry = toga.TextInput(style=my_style)
-        self.view = toga.MultilineTextInput(style=my_style)
+        self.view = toga.MultilineTextInput(style=my_style, placeholder="введите заметку")
         self.btn = toga.Button("Добавить", on_press=self.add_text, style=my_style)
         main_box.add(self.entry)
         main_box.add(self.view)
